2efa39ff0caec9b0a477b9667636bc9045c2a5cf
tylerwalt902 <51471968+tylerwalt902@users.noreply.github.com>
2019-06-07 22:52:28 +0545

    Update Content.js

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -9,8 +9,12 @@ type Props = {
 
 const Content = ({ body, title }: Props) => (
   <div className={styles['content']}>
-    <h1 className={styles['content__title']}>{title}</h1>
+    <h1 className={styles['content__title']}>Watch One Piece {title} Online.</h1>
     <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
+      <div className={styles['content__body']}>
+      Watch One Piece {title} online in high quality. Watch One Piece {title} Subbed Online. Watch One Piece {title} in best quality. 
+      Watch {title} of one piece in best quality. Download One Piece {title} in high quality.
+    </div>
   </div>
 );
 
