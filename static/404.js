bee78a7f8572e68c3ae2cf2a24f6dc45e2ebc774
tylerwalt902 <51471968+tylerwalt902@users.noreply.github.com>
2019-06-07 23:26:31 +0545

    Create 404.js

---
+++ static/404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const NotFoundPage = () =>
+  <div>
+    <h1>NOT FOUND</h1>
+    <Link to={`/`}>Click here to go home</Link>
+  </div>;
+
+export default NotFoundPage;
