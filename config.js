b7f7467aeab4543f2d6cd06233f0cdaafe1e032c
tylerwalt902 <51471968+tylerwalt902@users.noreply.github.com>
2019-06-07 23:38:35 +0545

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   subtitle: 'Watch latest one piece episodes for free online in high quality updated weekly in HD.',
   copyright: '© 1Piece.me. All rights reserved. Copyrights and trademarks for the anime, and other promotional materials are held by their respective owners and their use is allowed under the fair use clause of the Copyright Law.',
   disqusShortname: '1Pieceme',
-  postsPerPage: 1,
+  postsPerPage: 50,
   googleAnalyticsId: 'UA-58419551-5',
   menu: [
     {
